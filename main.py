e86318eb5f6eb850c0b2a39b80a0c28ab5b470aa
gmdiniz <gabrieldinizmota@hotmail.com>
2022-12-14 21:44:55 -0300

    This commit clean the code structure

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ async def launch(websites_list):
     data_list = [response for response in response_list]
     return data_list
 
+
 def main():
     websites_list = []
     website_info = []
@@ -31,24 +32,21 @@ def main():
     
     websites_content = asyncio.run(launch(websites_list))
 
-    # Parse the response content
     for site_content in websites_content:
         if site_content.status_code  != 200:
             sys.stderr.write(str({'Error code': site_content.status_code})) 
         parsed_site = str(BeautifulSoup(site_content, 'html.parser')) 
     
-        # Match the urls and phone in lxml using regex pattern
         match_url_list = [url[0] for url in re.findall(URL_PATTERN, parsed_site)]
         all_number_found = re.findall(PHONE_NUMBER_PATTERN, parsed_site)
-        match_phone = set(filter(lambda x: len(x) >= MIN_NUMBER_LENGHT, all_number_found))
 
-        # Filter the urls by the image format list
         image_url_list = set([url for url in match_url_list if any(extension in url for extension in IMAGE_FORMAT_LIST)])
 
-        # Format the phone numbers result
+        match_phone = set(filter(lambda x: len(x) >= MIN_NUMBER_LENGHT, all_number_found))
             
         website_info.append({ 'logo': image_url_list, 'phone': match_phone, 'website': str(site_content.url) })
         sys.stdout.write(str(website_info))
+
 
 if __name__ == "__main__":
     main()
